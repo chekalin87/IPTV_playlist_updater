a8f0aa287a80cc273ba78a844b24711a4292502d
cloud011010 <chekalin87@gmail.com>
2020-03-05 01:45:42 +0200

    Теперь можно обновляться с нескольких ресурсов, в следующем коммите опишу подробнее, особо не тестировал

--- main.py
+++ main.py
@@ -1,51 +1,92 @@
 import requests
 import os
 
-
-def find_chanels(name):
-    i = 0
-    for line in source:
-        i += 1
-        if line.strip() == name.strip():
-            address = source[i]
-
-            return address
-    return "\n"
+channels = {}
 
 
-def update():
+def create_playlist():
+    all_channels_file = open('all_channels.m3u', "w", encoding="utf-8")
+    for key in channels:
+        key = key.strip()
+        channels[key] = channels[key].strip()
+        all_channels_file.write(key + "\n")
+        all_channels_file.write(channels[key] + "\n")
+    all_channels_file.close()
     list_file = open('list.txt', "r", encoding="utf-8")
+
+
     out_file = open('iptvchannels.m3u', "w", encoding="utf-8")
     out_file.write("#EXTM3U\n")
-
-    for line in list_file.readlines():
-        out_file.write(line)
-        out_file.write(find_chanels(line))
+    for line in list_file:
+        if "#EXTINF:" in line:
+            for key in channels:
+                if key.strip() == line.strip():
+                    out_file.write(key + "\n")
+                    out_file.write(channels[key] + "\n")
 
     list_file.close()
     out_file.close()
 
 
+def update():  # Создаёт общий словарь с каналами channels
+    stuck = []
+    i = 0
+    list_file = open('list.txt', "r", encoding="utf-8")
+    stuck = list_file.readlines()
+    list_file.close()
+    while os.path.exists('sources/' + str(i) + '.txt'):
+        source_file_r = open('sources/' + str(i) + '.txt', "r", encoding="utf-8")
+        stuck = stuck + source_file_r.readlines()
+        source_file_r.close()
+        i += 1
+
+    for line in stuck:
+        if "#EXTINF:" in line:
+            if "#EXTGRP:" in stuck[stuck.index(line) + 1]:
+                channels[line.strip()] = stuck[stuck.index(line) + 2]
+            else:
+                channels[line.strip()] = stuck[stuck.index(line) + 1]
+
+    print(stuck)
+
+
+def search_links():
+    try:
+        links_file = open('links.txt', "r", encoding="utf-8")
+    except:
+        print("Не смог открыть файл links.txt")
+    else:
+        return links_file.readlines()
+    finally:
+        links_file.close()
+
+
 def downloading_playlists():
     try:
-        ufr = requests.get("https://smarttvnews.ru/apps/iptvchannels.m3u")
-    except:
-        print("Сервис не отвечает")
-    else:
-        print("скчал плейлист с https://smarttvnews.ru/apps/iptvchannels.m3u")
-
-    try:
-        os.mkdir("source")
+        os.mkdir("sources")
     except:
         pass
     else:
-        print("Нет папки \"source\", создаю...")
-    source_file = open('source/source.txt', "wb")
-    source_file.write(ufr.content)
-    source_file.close()
+        print("Нет папки \"sources\", создаю...")
+    i = 0
+    while i < len(links):
+        try:
+            ufr = requests.get(links[i].strip())
+        except:
+            print("Сервис " + links[i] + " не отвечает")
+        else:
+            print("скчал плейлист с " + links[i])
+        source_file = open('sources/' + str(i) + '.txt', "wb")
+        source_file.write(ufr.content)
+        source_file.close()
+        i += 1
 
+
+links = search_links()
 downloading_playlists()
-source_file_r = open('source/source.txt', "r", encoding="utf-8")
-source = source_file_r.readlines()
 update()
-source_file_r.close()
+create_playlist()
+print(channels)
+print("END")
+
+
