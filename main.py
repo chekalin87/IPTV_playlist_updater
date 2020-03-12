5694f55fb82954d7fae022d47e191701c92dfad2
cloud011010 <chekalin87@gmail.com>
2020-03-12 23:35:26 +0200

    Начал рисовать интерфейс

--- main.py
+++ main.py
@@ -1,137 +1,80 @@
-import sys
-import requests
-import os
+# -*- coding: utf-8 -*-
 
-channels = {}
-not_finded_channels =[]
+import math
+import tkinter as tk
 
-def close_program(code=0):
-    input("\nВведи букву Хуй чтобы выйти: ")
-    sys.exit(code)
+root = tk.Tk()
+root.title("PLUG v G0.1")
+root.geometry("800x800+2000+100")
 
 
-def create_playlist():
-    list = []
-    all_channels_file = open('all_channels.m3u', "w", encoding="utf-8")
-    all_channels_file.write("#EXTM3U\n")
-
-    for key, value in sorted(channels.items()):
-        key = key.strip()
-        value = value.strip()
-        all_channels_file.write(key + "\n")
-        all_channels_file.write(value + "\n")
-    all_channels_file.close()
-
-    try:
-        list_file = open('list.txt', "r", encoding="utf-8")
-    except:
-        list_file = open('list.txt', "w", encoding="utf-8")
-        print("Нет файла \"list.txt\"...\nне переживай, я его уже создал,\nтебе нужно записать туда\nканалы со ссылками, короче ты понял.")
-        close_program(1)
-    else:
-        list = list_file.readlines()
-    finally:
-        list_file.close()
-    if list == []:
-        print("Ну бля...\nпустой \"list.txt\"...\nТы читал README?!!!")
-        close_program(1)
-
-    out_file = open('OUT.m3u', "w", encoding="utf-8")
-    out_file.write("#EXTM3U\n")
-    for line in list:
-        find_channel = False
-        if "#EXTINF:" in line:
-            splited_line = line.split(",", 1)
-            formating_line = "#EXTINF:-1," + splited_line[1].strip()
-            for key, value in channels.items():
-                if key == formating_line:
-                    out_file.write(key + "\n")
-                    out_file.write(value.strip() + "\n")
-                    find_channel = True
-            if not find_channel:
-                not_finded_channels.append(formating_line)
-                out_file.write(line.strip() + "\n")
-                out_file.write(list[list.index(line)+1].strip() + "\n")
-    out_file.close()
+def get_geometry():
+    """Returns the tuple: (<width>, <height>, <width_position>, <height_position>)
+    можно вот так: print(get_geometry()[2])"""
+    root.update_idletasks()
+    string = root.geometry()
+    size, pos = string.split("+", 1)
+    x, y = size.split("x")
+    px, py = pos.split("+")
+    x = int(x)
+    y = int(y)
+    px = int(px)
+    py = int(py)
+    return x, y, px, py
 
 
-def update():  # Создаёт общий словарь с каналами channels
-    stuck = []
-
-    for file in os.listdir("sources/"):
-        source_file_r = open('sources/' + file, "r", encoding="utf-8")
-        stuck = stuck + source_file_r.readlines()
-        source_file_r.close()
-    for line in stuck:
-        if "#EXTINF:" in line:
-            splited_line = line.split(",", 1)
-            formating_line = "#EXTINF:-1," + splited_line[1].strip()
-            if "#EXTGRP:" in stuck[stuck.index(line) + 1]:
-                channels[formating_line.strip()] = stuck[stuck.index(line) + 2]
-            else:
-                channels[formating_line.strip()] = stuck[stuck.index(line) + 1]
+def add_channel(event):
+    """ЗАГЛУШКА"""
+    channel_list.insert(tk.END, name_entry.get() + ">>>" + addr_entry.get())
+    print("Шо я делаю?")
 
 
-def search_links():
-    try:
-        links_file = open('links.txt', "r", encoding="utf-8")
-    except:
-        links_file = open('links.txt', "w", encoding="utf-8")
-        print(
-            "Нет файла \"links.txt\"...\nне переживай, я его уже создал,\nтебе нужно записать туда хоть одну ссылку на плейлист.")
-        close_program(1)
-    else:
-        links_list = links_file.readlines()
-        if links_list == []:
-            print("\"links.txt\"пуст,\nтебе нужно записать туда хоть одну ссылку на плейлист.")
-            close_program(1)
-        return links_list
-    finally:
-        links_file.close()
+def change_name(event):
+    """ЗАГЛУШКА"""
+    print(name_entry.get())
 
 
-def downloading_playlists():  # Качает (перезаписывает) файлы исходных плейлистов
-    try:
-        os.mkdir("sources")
-    except:
-        pass
-    else:
-        print("Нет папки \"sources\", создал")
-
-    files = os.listdir("sources")
-    for var in files:
-        try:
-            os.remove("sources/" + var)
-        except:
-            print("Не могу удалить: sources/" + var)
-        else:
-            print("удалил: sources/" + var)
-
-    for i in range(len(links)):
-        link = links[i].strip()
-        sp_l = link.split("/")
-        name = sp_l[-1]
-        try:
-            ufr = requests.get(link)
-        except:
-            print("Сервис " + link + " не отвечает")
-        else:
-            source_file = open("sources/" + name, "wb")
-            source_file.write(ufr.content)
-            print("скчал плейлист " + link)
-        finally:
-            source_file.close()
+def change_addr(event):
+    """ЗАГЛУШКА"""
+    print(addr_entry.get())
 
 
-links = search_links()
-downloading_playlists()
-update()
-create_playlist()
+top_frame = tk.Frame(root, bg="green", height=30)
 
-if not_finded_channels != []:
-    print("Каналы:")
-    for i in not_finded_channels:
-        print(i)
-    print("не были найдены.")
-    print("Это конец ;)")
-close_program(0)
+list_frame = tk.Frame(root, bg="white")
+channel_list = tk.Listbox(list_frame)
+scroll = tk.Scrollbar(list_frame)
+
+sort_frame = tk.Frame(root)
+add_btn = tk.Button(sort_frame, text="add")
+add_btn.bind("<Button-1>", add_channel)
+up_btn = tk.Button(sort_frame, text="/\\")
+down_btn = tk.Button(sort_frame, text="\/")
+del_btn = tk.Button(sort_frame, text="del")
+
+edit_frame = tk.Frame(root)
+name_label = tk.Label(edit_frame, text="Название:")
+name_entry = tk.Entry(edit_frame)
+name_entry.bind("<Return>", change_name)
+addr_label = tk.Label(edit_frame, text="Адрес:")
+addr_entry = tk.Entry(edit_frame)
+addr_entry.bind("<Return>", change_addr)
+
+# packs
+top_frame.pack(side=tk.TOP, fill="x", expand=False)
+list_frame.pack(side=tk.LEFT, fill="both", expand=True)
+channel_list.pack(side=tk.LEFT, fill="both", expand=True)
+scroll.pack(side=tk.RIGHT, fill="both")
+sort_frame.pack()
+add_btn.pack(side=tk.LEFT)
+up_btn.pack(side=tk.LEFT)
+down_btn.pack(side=tk.LEFT)
+del_btn.pack(side=tk.LEFT)
+edit_frame.pack()
+name_label.pack()
+name_entry.pack()
+addr_label.pack()
+addr_entry.pack()
+
+
+root.mainloop()
